f840a6e49d18b968936fa83a42264e4bf1d2fbc6
Julio Cesar Martinez Gambino <julio98az@gmail.com>
2018-08-24 19:41:13 -0700

    Menu de pausa agregado
    con boton de continuar y boton para reiniciar  juego

--- PracticasDDI/Assets/Scripts/Pauser.cs
+++ PracticasDDI/Assets/Scripts/Pauser.cs
@@ -1,14 +1,15 @@
 ﻿using UnityEngine;
 using System.Collections;
-
+using UnityEngine.Scene;
 public class Pauser : MonoBehaviour {
 	private bool paused = false;
-	
+    public GameObject pausePanel;
 	// Update is called once per frame
 	void Update () {
 		if(Input.GetKeyUp(KeyCode.P))
 		{
 			paused = !paused;
+            pausePanel.SetActive(paused);
 		}
 
 		if(paused)
@@ -16,4 +17,19 @@ public class Pauser : MonoBehaviour {
 		else
 			Time.timeScale = 1;
 	}
+    public void ContinueGame()
+    {
+        Debug.Log("Continuaar...");
+        paused = false;
+        pausePanel.SetActive(paused);
+    }
+    public void ResetGame()
+    {
+        Debug.Log("Continuaar");
+        SceneManager.LoadScene(0);
+    }
+    public void MuteMusic()
+    {
+        Debug.Log("Continuaar");
+    }
 }
