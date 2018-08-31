5f2bcab15cbc67901992d3df2de657b973dcc105
Julio Cesar Martinez Gambino <julio98az@gmail.com>
2018-08-31 16:51:25 -0700

    Menu de pausa agregado

--- PracticasDDI/Assets/Scripts/Pauser.cs
+++ PracticasDDI/Assets/Scripts/Pauser.cs
@@ -1,35 +1,55 @@
 ﻿using UnityEngine;
 using System.Collections;
-using UnityEngine.Scene;
-public class Pauser : MonoBehaviour {
-	private bool paused = false;
-    public GameObject pausePanel;
-	// Update is called once per frame
-	void Update () {
-		if(Input.GetKeyUp(KeyCode.P))
-		{
-			paused = !paused;
-            pausePanel.SetActive(paused);
-		}
+using UnityEngine;
+using UnityEngine.SceneManagement;
 
-		if(paused)
-			Time.timeScale = 0;
-		else
-			Time.timeScale = 1;
-	}
+public class Pauser : MonoBehaviour
+{
+    public string mainMenuScene;
+    private bool paused = false;
+    public GameObject pausePanel;
+    public GameObject audio;
+    public bool mute;
+    // Update is called once per frame
+
+
+
+    void Update()
+    {
+        if (Input.GetKeyUp(KeyCode.P))
+        {
+            paused = !paused;
+            pausePanel.SetActive(paused);
+
+        }
+        if (paused)
+            Time.timeScale = 0;
+
+        else
+            Time.timeScale = 1;
+
+
+    }
+
     public void ContinueGame()
     {
-        Debug.Log("Continuaar...");
+        Debug.Log("Continuar");
         paused = false;
         pausePanel.SetActive(paused);
+       
     }
     public void ResetGame()
     {
-        Debug.Log("Continuaar");
-        SceneManager.LoadScene(0);
+
+        Debug.Log("ResetGame");
+        SceneManager.LoadScene(1);
+
     }
     public void MuteMusic()
     {
-        Debug.Log("Continuaar");
+
+        Debug.Log("unMusicGame");
+        mute = !mute;
+        audio.SetActive(mute);
     }
 }
