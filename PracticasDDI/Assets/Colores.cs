922d774c34462fd40990e7bf940183f687f8cb92
Julio Cesar Martinez Gambino <julio98az@gmail.com>
2018-09-14 22:38:24 -0700

    Objetos agregados
    2 objetos fueron agregados, uno al acercarse se mostrara una imagen con un audio, miestras que el otro al presionar la letra c deberia cambiar de color, aunque por falta de tiempo no se pudo avirguarel motivo que no funsiona

---
+++ PracticasDDI/Assets/Colores.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Colores : MonoBehaviour
+{
+    public bool esColores = false;
+    private Renderer colorEsfera;
+    public UnityEngine.Color color;
+    void start()
+    {
+        colorEsfera = GetComponent<Renderer>();
+        colorEsfera.material.color=color;
+        esColores = true;
+    }
+    void uptade()
+    {
+        if (Input.GetKeyUp(KeyCode.C))
+        {
+            colorChange();
+        }
+
+    }
+
+    public void colorChange()
+    {
+        if (esColores == true)
+        {
+            colorEsfera.material.color = UnityEngine.Color.red;
+            esColores = false;
+        }
+        else
+        {
+            colorEsfera.material.color=UnityEngine.Color.blue;
+            esColores = true;
+        }
+    }
+}
---
+++ PracticasDDI/Color.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Color : MonoBehaviour
+{
+    public Color color;
+    public bool esColor = false
+    public GameObject infoPanel;
+    public GameObject audio;
+    private bool isPlayerInside = false;
+    public float rotationSpeed = 3f;
+
+    private void Start()
+    {
+        infoPanel.SetActive(false);
+    }
+
+    private void Update()
+    {
+        transform.Rotate(Vector3.up * Time.deltaTime * rotationSpeed);
+
+        if (Input.GetKeyDown(KeyCode.I) && isPlayerInside)
+        {
+            gameObject.GetComponent<Renderer>().material.color = color;
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            audio.SetActive(true);
+            infoPanel.SetActive(true);
+            isPlayerInside = true;
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            audio.SetActive(false);
+            infoPanel.SetActive(false);
+            isPlayerInside = false;
+        }
+    }
+}
