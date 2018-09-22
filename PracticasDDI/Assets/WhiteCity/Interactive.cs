2af1dae5f619d0b61cb6a6359806bf8fa55a517a
Julio Cesar Martinez Gambino <julio98az@gmail.com>
2018-09-21 21:46:11 -0700

    Nuevos objetos
    " onjetos se agregaron que cuando se entra a su sphere collider desaparecen y un nuevo item se agrega
    Nota: El programa tiende a pausarse solo y los objetos no podran ser collectados hasta que se entre al objeto una vez, se salga, y se vuelva a ingresar

---
+++ PracticasDDI/Assets/WhiteCity/Interactive.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Interactive : MonoBehaviour
+{
+    // Use this for initialization
+
+    //Sprite InventoriSprite
+    private bool isPlayerInside = false;
+
+    public GameObject interactivePanel;
+    public GameObject infoPanel;
+
+
+    public void Start()
+    {
+        infoPanel.SetActive(false);
+        interactivePanel.SetActive(false);
+    }
+    public void Update()
+    {
+
+        transform.Rotate(Vector3.up * Time.deltaTime);
+
+        if (Input.GetKeyDown(KeyCode.N) && isPlayerInside)
+        {
+            FindObjectOfType<inventory>().addNewItem();//(invetorySprite)
+            Destroy(this.gameObject);
+        }
+
+        if (Input.GetKeyDown(KeyCode.I) && isPlayerInside)
+        {
+            interactivePanel.SetActive(true);
+            isPlayerInside = true;
+        }
+    }
+
+    public void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            infoPanel.SetActive(true);
+            isPlayerInside = true;
+        }
+
+    }
+
+    public void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            infoPanel.SetActive(false);
+            isPlayerInside = false;
+        }
+    }
+}
---
+++ PracticasDDI/Assets/WhiteCity/inventory.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class inventory : MonoBehaviour
+{
+
+    public GameObject inventoryPanel;
+
+    public GameObject backgroundPanel;
+
+    public Sprite SwordSprite; //lista de items
+
+
+    public void addNewItem()
+    {
+        GameObject item = new GameObject();
+        item.transform.SetParent(inventoryPanel.transform);
+        Image image = item.AddComponent<Image>();
+        image.sprite = SwordSprite;
+    }
+
+    public void Start()
+    {
+        backgroundPanel.SetActive(false);
+    }
+
+    public void Update()
+    {
+        if (Input.GetKey(KeyCode.B))
+        {
+            backgroundPanel.SetActive(!backgroundPanel.activeSelf);
+        }
+    }
+}
