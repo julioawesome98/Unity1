0b9caf35027f79da4f98b7a7f848ef1697a97641
Julio Cesar Martinez Gambino <julio98az@gmail.com>
2018-10-22 00:11:59 -0700

    Laberinto agregado
    Se agrego laberinto que sera usado vr

---
+++ PracticasDDI/Assets/GyroCamera.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GyroCamera : MonoBehaviour {
+    public Gyroscope gyro;
+    public bool gyroSupported;
+	// Use this for initialization
+	void Start () {
+        gyroSupported = SystemInfo.supportsGyroscope;
+        if (gyroSupported)
+        {
+        gyro = Input.gyro;
+        gyro.enabled = true;
+        }
+         
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.rotation=gyro.attitude;
+	}
+}
