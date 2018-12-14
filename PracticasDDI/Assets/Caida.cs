9ea8bf7c7efaeae10fa280b10bfc6c3ea4499efa
Unknown <jmartinez36@uabc.edu.mx>
2018-12-13 17:45:18 -0800

    Version 2
    Pelota y script fueron agregados para generar la bola, se esta teniendo probelmas en spawniar la bola

---
+++ PracticasDDI/Assets/Caida.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Caida : MonoBehaviour {
+    public GameObject plane;
+
+    public GameObject spawnpoint;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (transform.position.y < plane.transform.position.y - 10) 
+        {
+            transform.position = spawnpoint.transform.position;
+        }
+	}
+}
